e3a470de2b83b0ebac8dd7e6730c4babd7342819
Caio <caio.gabriel@deway.com.br>
2019-11-20 16:59:26 -0300

    ajeitei a tela movies

--- src/pgs/movie.js
+++ src/pgs/movie.js
@@ -1,15 +1,20 @@
-import React, { Component } from "react";
-import {View ,Text} from "react-native";
+import React from "react";
+import {View ,Text , Image} from "react-native";
+
+const Movie = ({ navigation }) => {
+    return(
+        <View>
+            <Image 
+            style = {{ flex:1, }}
+            source={{uri: `https://image.tmdb.org/t/p/original${navigation.state.params.movie.poster_path}`}}
+            />
+            <Text>{navigation.state.params.movie.overview}</Text>
+        </View>
+    );
+} 
+
 
 Movie.navigationOptions = ({ navigation }) =>({
     title: navigation.state.params.movie.title
 });
-
-export default class Movie extends Component{
-    render(){
-        return(
-    <View>
-        <Text>{navigation.state.params.movie.overview}</Text>
-    </View>
-        )}
-};
+export default Movie;
