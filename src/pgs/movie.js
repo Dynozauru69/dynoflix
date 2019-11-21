d4a974e8f992284e32407277fbac2adf09951149
Caio <caio.gabriel@deway.com.br>
2019-11-21 16:59:11 -0300

    tela movie treminada,modifiquei icone e ajustei app.json para buildar

--- src/pgs/movie.js
+++ src/pgs/movie.js
@@ -1,14 +1,46 @@
 import React from "react";
-import {View ,Text , Image} from "react-native";
+import {View ,Text , Image ,StyleSheet} from "react-native";
 
 const Movie = ({ navigation }) => {
     return(
-        <View>
+        <View
+        style = {
+            {
+                flex: 1,
+                padding: 20,
+                borderRadius: 5,
+                borderColor: '#DDD',
+                borderWidth: 1,
+                backgroundColor: '#FFF',
+                marginBottom: 10,
+                marginTop: 10,
+            }
+        }
+        >
             <Image 
-            style = {{ flex:1, }}
-            source={{uri: `https://image.tmdb.org/t/p/original${navigation.state.params.movie.poster_path}`}}
+            style = {{ 
+                        height: 300, 
+                        width: 200, 
+                        alignSelf: 'center', 
+                        marginBottom: 10,
+                    }}
+            source={{uri: `https://image.tmdb.org/t/p/w200${navigation.state.params.movie.poster_path}`}}
             />
-            <Text>{navigation.state.params.movie.overview}</Text>
+            <Text
+            style = {{marginBottom: 5,}}
+            >{navigation.state.params.movie.overview}</Text>
+            <Text
+            style = {{
+                color: 'grey',
+                marginBottom: 5,
+        }}
+            >Avalição: {navigation.state.params.movie.vote_average}</Text>
+            <Text
+            style = {{
+                color: 'grey',
+                marginBottom: 5,
+            }}
+        >Popularidade: {navigation.state.params.movie.popularity}</Text>
         </View>
     );
 } 
